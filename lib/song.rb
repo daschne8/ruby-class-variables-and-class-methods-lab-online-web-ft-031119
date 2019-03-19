6b94bdaee1c25b2837b3807c539d93f7e58af2b1
Daniel Schneider <daschne8@gmail.com>
2019-03-19 15:41:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
   attr_reader :name,:artist,:genre
   @@artists = []
   @@genres = []
+  @@count = 0
   def self.new(name,artist,genre)
     @name = name
     @artist = artist
     @@artists << artist
     @genre = genre
     @@genres << genre
+    @@count += 1
   end
 
   def self.genre_count
