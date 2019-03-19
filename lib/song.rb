d128889ddb8bccf86d851e468ab7b8d2813c88fa
Daniel Schneider <daschne8@gmail.com>
2019-03-19 15:36:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,15 @@
 class Song
+  attr_accessor
+  attr_reader :name,:artist,:genre
+  @@artists = []
+  @@genres = []
   def new(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+end
+
+  
     
